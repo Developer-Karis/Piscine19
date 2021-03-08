c5cdadd4ac45731813631295ebdba33f84959e17
Developer-Karis <papadopouloskaris@gmail.com>
2021-03-08 10:24:13 +0100

    C01 finish

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kpapadop <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/07 19:27:47 by kpapadop          #+#    #+#             */
-/*   Updated: 2021/03/08 09:51:55 by kpapadop         ###   ########.fr       */
+/*   Created: 2021/03/08 09:52:50 by kpapadop          #+#    #+#             */
+/*   Updated: 2021/03/08 10:18:47 by kpapadop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_rev_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int *tab, int size)
 {
 	int i;
+	int j;
+	int temp;
 
-	i = size - 1;
-	while (i >= 0)
+	i = 0;
+	while (i < size)
 	{
-		i--;
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
 	}
 }
