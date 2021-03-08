2bc22399414198322d47afecda6e658a4f31e1a9
Developer-Karis <papadopouloskaris@gmail.com>
2021-03-08 15:54:20 +0100

    ex07 error fixed

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpapadop <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/08 11:28:32 by kpapadop          #+#    #+#             */
+/*   Updated: 2021/03/08 12:22:18 by kpapadop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+void	*ft_strcpy(char *dest, char *src)
+{
+	strcpy(*dest, *src);
+}
+
+int		main(void)
+{
+	char str1[] = "Hello";
+	char str2[] = "World";
+
+	*ft_strcpy(str1, str2);
+	printf("%c\t", str1);
+	printf("%c\t", str2);
+	return (0);
+}
